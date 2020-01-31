d8f353360630412947231958b11a5dce9e2f487b
shoukugeki02 <59277996+shoukugeki02@users.noreply.github.com>
2020-01-31 12:39:14 +0700

    Update lab9_3.cpp

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,29 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+
+int main (){
+    int count=0;
+    float sum=0;
+    float x =0;
+    string textline;
+    ifstream source("score.txt");
+    while(getline(source,textline))
+    {
+        cout << textline << "\n";
+        sum += atof(textline.c_str());
+        x += pow(atof(textline.c_str()),2);
+        
+        count++;
+    }
+    cout << "Number of data = "<< count <<"\n";
+    cout << "Mean = "<< sum/count << "\n";
+    cout << "Standard deviation = "<< sqrt(x/count-pow(sum/count,2 ));
+    source.close();
+    return 0;
+
+
+}
