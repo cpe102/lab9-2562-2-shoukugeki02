b386db3a4e7d6279006a0848b8d5a37e1073304e
shoukugeki02 <59277996+shoukugeki02@users.noreply.github.com>
2020-01-31 12:38:28 +0700

    Update lab9_2.cpp

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,4 +1,24 @@
-//cout << "-------------------- SOTUS ---------------------";
-//cout << textline;
-//cout << "-------------------- SOTUS ---------------------";
-//Don't forget to change cout to output filestream
+#include<iostream>
+#include<fstream>
+#include <string>
+using namespace std;
+
+int main (){
+    ifstream source;
+    ofstream dest;
+    string textline;
+     source.open("cheerbook.txt");
+     dest.open("cheerbook_copy.txt" );
+    dest << "-------------------- SOTUS ---------------------" << "\n";
+   while(getline(source,textline))
+   {
+      dest << textline << endl ;
+              
+   
+   }
+      dest << "-------------------- SOTUS ---------------------";
+      source.close();
+      dest.close();
+      return 0;
+
+}
